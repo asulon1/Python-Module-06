d7f158855a3ffae37a827186314579c7ba9a37a3
asulon <asulon@student.42.fr>
2026-03-23 17:42:40 +0100

    init part0

---
+++ alchemy/elements.py
@@ -0,0 +1,26 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  element.py                                        :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: asulon <asulon@student.42.fr>             +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/03/23 15:02:56 by asulon          #+#    #+#               #
+#  Updated: 2026/03/23 15:04:49 by asulon          ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+def create_fire() -> str:
+    return "Fire element created"
+
+
+def create_water() -> str:
+    return "Water element created"
+
+
+def create_earth() -> str:
+    return "Earth element created"
+
+
+def create_air() -> str:
+    return "Air element created"
---
+++ ft_sacred_scroll.py
@@ -0,0 +1,46 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  ft_sacred_scroll.py                               :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: asulon <asulon@student.42.fr>             +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/03/23 15:02:48 by asulon          #+#    #+#               #
+#  Updated: 2026/03/23 16:01:52 by asulon          ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+import alchemy
+
+
+def main():
+    print("=== Sacred Scroll Mastery ===\n")
+    try:
+        print("Testing direct module access:")
+        print(
+            f"alchemy.elements.create_fire(): {alchemy.elements.create_fire()}")
+        print(
+            f"alchemy.elements.create_fire(): {alchemy.elements.create_water()}")
+        print(
+            f"alchemy.elements.create_earth(): {alchemy.elements.create_earth()}")
+        print(
+            f"alchemy.elements.create_air(): {alchemy.elements.create_air()}\n")
+    except AttributeError as error:
+        print(f"Error: While create elements : {error.name}")
+
+    print("Testing package-level access (controlled by __init__.py):")
+    print(f"alchemy.create_fire(): {alchemy.create_fire()}")
+    print(f"alchemy.create_water(): {alchemy.create_water()}")
+
+    try:
+        print(f"alchemy.create_earth(): {alchemy.create_earth()}")
+    except AttributeError:
+        print("alchemy.create_earth(): AttributeError - not exposed")
+    try:
+        print(f"alchemy.create_air(): {alchemy.create_air()}")
+    except AttributeError:
+        print("alchemy.create_air(): AttributeError - not exposed")
+
+
+if __name__ == "__main__":
+    main()
