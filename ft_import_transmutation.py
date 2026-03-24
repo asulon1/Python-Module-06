548b27432a4544dec80c54c9f3d8a4f83c0d47bf
asulon <asulon@student.42.fr>
2026-03-24 12:06:00 +0100

    finish part2

--- ft_import_transmutation.py
+++ ft_import_transmutation.py
@@ -6,13 +6,14 @@
 #  By: asulon <asulon@student.42.fr>             +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/03/23 18:02:42 by asulon          #+#    #+#               #
-#  Updated: 2026/03/23 18:10:01 by asulon          ###   ########.fr        #
+#  Updated: 2026/03/24 12:05:04 by asulon          ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
 import alchemy
-from alchemy import create_water
-from alchemy.potions import healing_potion as heal
+from alchemy.elements import create_water
+from alchemy.potions import healing_potion as heal, strength_potion
+from alchemy.elements import create_earth, create_fire
 
 
 def main():
@@ -23,10 +24,17 @@ def main():
         f"alchemy.elements.create_fire(): {alchemy.elements.create_fire()}\n")
 
     print("Method 2 - Specific function import:")
-    print(f"create_water(): {create_water()}")
+    print(f"create_water(): {create_water()}\n")
 
     print("Method 3 - Aliased import:")
-    print(f"heal(): {heal()}")
+    print(f"heal(): {heal()}\n")
+
+    print("Method 4 - Multiple imports:")
+    print(f"create_earth(): {create_earth()}")
+    print(f"create_fire(): {create_fire()}")
+    print(f"strength_potion(): {strength_potion()}\n")
+
+    print("All import transmutation methods mastered!")
 
 
 if __name__ == "__main__":
